ea96955f166a7ec91a36606f7db8eed54ca6c234
Hector-bit <hectormartinez.2978@gmail.com>
2019-11-01 09:57:52 -0700

    all CRUD operations work for projects and actions

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ I need this code, just don't know where, perhaps should make some middleware, do
 Go code!
 */
 
+const projects = require('./data/helpers/projectModel');
+const actions = require('./data/helpers/actionModel');
+
 const express = require('express');
 const server = express();
-const projects = require('./data/helpers/projectModel')
 
 server.use(express.json());
 
@@ -44,6 +46,106 @@ server.get('/api/projects/:id', (req, res) => {
     })
     .catch(err => res.status(400).json('bad request'))
 })
+
+server.get('/api/projects/:id/actions', (req, res) => {
+    projects.getProjectActions(req.params.id)
+    .then(action => {
+        if(action){
+            res.status(200).json(action);
+        } else {
+            res.status(404).json('this project does not exist')
+        }
+    })
+    .catch(err => res.status(400).json('bad request'))
+})
+
+server.post('/api/projects/', (req, res) => {
+    projects.insert(req.body)
+    .then(project => {
+        if(project){
+            res.status(200).json(project);
+        } else {
+            res.status(404).json('project was not added')
+        }
+    })
+    .catch(err => res.status(400).json('bad request'))
+})
+
+server.delete('/api/projects/:id', (req, res) => {
+    projects.remove(req.params.id)
+    .then(project => {
+        if(project){
+            res.status(200).json('project was deleted');
+        } else {
+            res.status(404).json('project was not deleted')
+        }
+    })
+    .catch(err => res.status(400).json('bad request'))
+})
+
+server.put('/api/projects/:id', (req, res) => {
+    projects.update(req.params.id, req.body)
+    .then(project => {
+        if(project){
+            res.status(200).json(project);
+        } else {
+            res.status(404).json('project was not updated')
+        }
+    })
+    .catch(err => res.status(400).json('bad request'))
+})
+
+//actions 
+
+server.get('/api/projects/:id/actions/:id', (req, res) => {
+    actions.get(req.params.id)
+    .then(action => {
+        if(action){
+            res.status(200).json(action);
+        } else {
+            res.status(404).json('this action does not exist')
+        }
+    })
+    .catch(err => res.status(400).json('bad request'))
+})
+
+server.post('/api/projects/:id/actions', (req, res) => {
+    actions.insert(req.body)
+    .then(action => {
+        if(action){
+            res.status(200).json(action);
+        } else {
+            res.status(404).json('action was not added')
+        }
+    })
+    .catch(err => res.status(400).json('bad request'))
+})
+
+server.delete('/api/projects/:id/actions/:id', (req, res) => {
+    actions.remove(req.params.id)
+    .then(action => {
+        if(action){
+            res.status(200).json('action was deleted');
+        } else {
+            res.status(404).json('action was not deleted')
+        }
+    })
+    .catch(err => res.status(400).json('bad request'))
+})
+
+server.put('/api/projects/:id/actions/:id', (req, res) => {
+    actions.update(req.params.id, req.body)
+    .then(action => {
+        if(action){
+            res.status(200).json(action);
+        } else {
+            res.status(404).json('action was not updated')
+        }
+    })
+    .catch(err => res.status(400).json('bad request'))
+})
+
+
 
 server.listen(4000, () => {
     console.log('<=== API IS ON ===>')
